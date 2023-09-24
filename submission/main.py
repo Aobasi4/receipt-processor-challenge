ec73585bedc724047356d8c35b2c984e1d84f35f
Amara Obasi <aobasi4@gmail.com>
2023-09-24 12:59:39 -0400

    finished functions

--- submission/main.py
+++ submission/main.py
@@ -1,9 +1,11 @@
 from flask import Flask,request
 import receipt_processor as rp
 
+app = Flask(__name__)
+
 @app.post("/receipts/process")
 def process_receipt():
-    return "a process"
+    return rs.process_receipt(request)
 @app.get("/receipts/{id}/points")
 def get_points(id):
-    return "some points"
+    return rs.get_points(id)
