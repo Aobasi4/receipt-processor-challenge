353dbd751e9d0dfa815261f6ce75412a1c73e40e
Amara Obasi <aobasi4@gmail.com>
2023-09-26 15:16:49 -0500

    fixed everything besides datetime functions

--- submission/main.py
+++ submission/main.py
@@ -1,11 +1,15 @@
-from flask import Flask,request
 import receipt_processor as rp
+from flask import Flask,request
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
 @app.post("/receipts/process")
 def process_receipt():
-    return rs.process_receipt(request)
+    return rp.process_receipt(request)
 @app.get("/receipts/{id}/points")
 def get_points(id):
-    return rs.get_points(id)
+    return rp.get_points(id)
